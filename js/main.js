446d48b7eeae26607e899c80d41978817a805314
Apoorv Choubey <theapoorvs1@gmail.com>
2017-10-08 21:53:04 +0530

    project add

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ $(document).ready(function main(){
     $(".anchor").addClass("animated fadeInDown");
 
     $(".pro1").hover(function(){
-    $(".pro1").toggleClass("dark-pro1");
-    $(".pro1-text").toggle();
+        $(".pro1").toggleClass("dark-pro1");
+        $(".pro1-text").toggle();
+    });
+
+    $(".pro2").hover(function(){
+        $(".pro2").toggleClass("dark-pro1");
+        $(".pro2-text").toggle();
     });
 
 });
